5b5eb4687432ecd6f9bda1f4bd24b548f3db0110
Ilya Malashenko <melax08@yandex.ru>
2023-03-01 23:57:12 +0400

    Create tag, ingredient, recipe models in recipes app

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'recipes.apps.RecipesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ backend/foodgram/recipes/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+
+User = get_user_model()
+
+MINIMUM_COOKING_TIME = 1
+
+
+class Ingredient(models.Model):
+    name = models.CharField('Название интгредиента', max_length=200)
+    measurement_unit = models.CharField('Единица измерения', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField('Название тега', max_length=200, unique=True)
+    color = models.CharField('Цвет', max_length=7, unique=True)
+    slug = models.SlugField('Слаг', max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='recipes',
+                               verbose_name='Автор')
+    name = models.CharField('Название рецепта', max_length=200)
+    image = models.ImageField('Изображение', upload_to='recipes/')
+    text = models.TextField('Описание рецепта')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='IngredientRecipe',
+                                         related_name='recipes',
+                                         verbose_name='Ингредиенты')
+    tags = models.ManyToManyField(Tag,
+                                  through='TagRecipe',
+                                  related_name='recipes',
+                                  verbose_name='Теги')
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(MINIMUM_COOKING_TIME)],
+        verbose_name='Время приготовления в минутах')
+    pub_date = models.DateTimeField('Дата создания рецепта', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date', '-pk')
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField('Количество')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f'Ингредиент {self.ingredient} для рецепта '
+                f'{self.recipe} в количестве {self.quantity}') # Тут подумать над ingredient.measurement_unit
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Тег: {self.tag} для рецепта {self.recipe}'
+
