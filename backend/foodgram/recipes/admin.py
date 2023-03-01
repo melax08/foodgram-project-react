43a6b7427debde4863a679c32d990b45052aff94
Ilya Malashenko <melax08@yandex.ru>
2023-03-02 00:13:04 +0400

    Add media and .DS_Store to gitignore, add media root to settings.py

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Recipe, Ingredient
+
+admin.site.register(Tag)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
