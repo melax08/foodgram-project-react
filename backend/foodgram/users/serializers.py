e0849207dd40e68a5083cfadfe0c8f872b60f06c
Ilya Malashenko <melax08@yandex.ru>
2023-03-02 21:12:40 +0400

    Add password field to user validator with encode password

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import User, Follow
 
 
 class UserSerializer(serializers.ModelSerializer):
-    is_subscribed = serializers.BooleanField(default=False)
+    is_subscribed = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True, max_length=150,
+                                     validators=[validate_password])
 
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name', 'is_subscribed')
+        fields = ('email', 'id', 'username',
+                  'first_name', 'last_name', 'is_subscribed', 'password')
+
+    def get_is_subscribed(self, obj):
+        return Follow.objects.filter(
+            user__username=self.context['request'].user,
+            author__username=obj.username).exists()
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
