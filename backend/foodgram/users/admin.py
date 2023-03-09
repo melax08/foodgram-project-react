18550fb6e297a3d21a9ab2141cfb28b3a18a0e4c
Ilya Malashenko <melax08@yandex.ru>
2023-03-09 20:26:17 +0400

    Add load ingredients migration

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Follow
 
 
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ('email', 'username')
 
 
-admin.site.register(User, UserAdmin)
+# @admin.register(User)
+# class CustomUserAdmin(UserAdmin):
+#     list_filter = ('email', 'username')
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Follow)
