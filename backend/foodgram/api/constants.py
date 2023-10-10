dbb3caac343eabc012ac78f5ba4cdeb5582f5b92
Ilya Malashenko <melax08@yandex.ru>
2023-10-10 20:44:28 +0400

    Small refactoring and update README.md

--- backend/foodgram/api/constants.py
+++ backend/foodgram/api/constants.py
@@ -1,3 +1,3 @@
 SHOPPING_CART_HEADER = 'Ваш список покупок:'
-SHOPPING_CART_FOOTER = 'Foodgram - лучший сайт с рецептами.'
-SHOPPING_CART_FILENAME = 'foodgram_shopping_list.txt'
+SHOPPING_CART_FOOTER = 'Лучший сайт с рецептами.'
+SHOPPING_CART_FILENAME = 'recipes_shopping_list.txt'
